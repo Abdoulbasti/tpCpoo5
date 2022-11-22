00c3730ce29a03ba9dd7d11132a14044b71d4e83
Abdoulbasti <mukailaabdoulbassit@gmail.com>
2022-11-22 01:44:42 +0100

    tp3.exo3 finished

--- tp3/src/exo3/Son.java
+++ tp3/src/exo3/Son.java
@@ -9,11 +9,15 @@ public class Son extends Dad {
     public static void f(C c, A a) {
         System.out.println("Son : C : A");
     }
-
     public static void main(String[] args) {
         f(new B(), new A());
         f(new D(), new A());
         f(new B(), new D());
         f(new A(), new C());
+
+        /*
+        Question 3
+        f(new C(), new C());
+        f(new C(), new B());*/
     }
 }
